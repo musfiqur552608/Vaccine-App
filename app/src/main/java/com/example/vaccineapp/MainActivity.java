acab1c5d14927d7c44a9e5d3e1ba276f476b51e2
Musfiqur Rahman <mushfiqur15-10872@diu.edu.bd>
2023-07-31 19:50:47 +0600

    Done

--- app/src/main/java/com/example/vaccineapp/MainActivity.java
+++ app/src/main/java/com/example/vaccineapp/MainActivity.java
@@ -1,14 +1,45 @@
 package com.example.vaccineapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements ItemClickListener{
+    private RecyclerView recyclerView;
+    private VaccineModel[] myListData;
+    private VaccineAdapter adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerView);
+        myListData = new VaccineModel[]{
+                new VaccineModel("Hepatitis B Vaccine",R.drawable.vaccine1),
+                new VaccineModel("Tetanus Vaccine",R.drawable.vaccine2),
+                new VaccineModel("Pneumococcal Vaccine",R.drawable.vaccine3),
+                new VaccineModel("Rotavirus Vaccine",R.drawable.vaccine4),
+                new VaccineModel("Measles Vaccine",R.drawable.vaccine5),
+                new VaccineModel("Cholera Vaccine",R.drawable.vaccine6),
+                new VaccineModel("Covid-19 Vaccine",R.drawable.vaccine7),
+        };
+
+        adapter = new VaccineAdapter(myListData);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
+
+        adapter.setClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view, int pos) {
+        Toast.makeText(this, "Vaccine name: "+myListData[pos].getTitle(), Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/com/example/vaccineapp/VaccineAdapter.java
+++ app/src/main/java/com/example/vaccineapp/VaccineAdapter.java
@@ -16,6 +16,12 @@ public class VaccineAdapter extends RecyclerView.Adapter<VaccineAdapter.MyViewHo
         this.listData = listData;
     }
 
+    public static ItemClickListener clickListener;
+
+    public void setClickListener(ItemClickListener clickListener) {
+        this.clickListener = clickListener;
+    }
+
     @NonNull
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -37,7 +43,7 @@ public class VaccineAdapter extends RecyclerView.Adapter<VaccineAdapter.MyViewHo
         return listData.length;
     }
 
-    public static class MyViewHolder extends RecyclerView.ViewHolder{
+    public static class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
 
         public ImageView imageView;
         public TextView textView;
@@ -47,6 +53,14 @@ public class VaccineAdapter extends RecyclerView.Adapter<VaccineAdapter.MyViewHo
 
             this.imageView = itemView.findViewById(R.id.vaccineImg);
             this.textView = itemView.findViewById(R.id.vaccineName);
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            if(clickListener != null){
+                clickListener.onClick(view, getAdapterPosition());
+            }
         }
     }
 
